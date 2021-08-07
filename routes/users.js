f0185643200afa132ed80cf2a241678ba78b91ae
Alice Nguyen <alice.n.69521@gmail.com>
2021-08-07 12:56:28 -0700

    Workshop Assignment 3

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,16 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+    User.find()
+    .then((users) => {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'application/json');
+        res.json(users);
+    })
+    .catch((err) => {
+        return next(err);
+    })
 });
 
 router.post('/signup', (req, res) => {
