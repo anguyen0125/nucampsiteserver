979c183f45a76b0438c22e9730fd39d48ed9c996
anguyen0125 <alice.n.69521@gmail.com>
2021-08-17 17:56:09 -0700

    .gitignore update

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '1839397639592862',
-        clientSecret: '7f115a6269590a100065d8248faa5475'
-    }
-}
